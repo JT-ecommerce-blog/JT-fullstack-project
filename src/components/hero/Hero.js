1454b844f14db46a2f74c790bfd71680882f247f
Terry Dinh <tedinh@gmail.com>
2018-10-17 12:30:45 -0500

    added hero text

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class Hero extends Component {
   render() {
     return (
-      <div>Test</div>
+      <div>
+        <h1>Start Your Journey</h1>
+        <p>Learn how we became creatives and how we both transitioned from one career into Development</p>
+        <button>Read Our Story</button>
+      </div>
     )
   }
 }
