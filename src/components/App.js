de1148df94e0e629725a1487c3a58b21a41bfd55
Jeff Horner <jeffnhorner@outlook.com>
2018-10-18 22:01:46 -0500

    began styling navigation and fixed the scss error I was having earlier. Just needed to import all the scss files into the new styles.scss file and then call it on the index.js component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 
 import Nav from './navigation/Nav.js';
-import Home from './homepage/Homepage.js';
 import Footer from './footer/Footer.js';
 
-import '../scss/App.scss';
+import '../scss/styles.scss';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Nav />
-        <Home />
         <Footer />
       </div>
     );
