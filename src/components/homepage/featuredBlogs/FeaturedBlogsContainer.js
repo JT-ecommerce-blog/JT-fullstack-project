d40ba1c8fe51f64872557c5d24afba27e165c188
Terry Dinh <tedinh@gmail.com>
2018-10-20 17:02:39 -0500

    featured buckets

--- src/components/homepage/featuredBlogs/FeaturedBlogsContainer.js
+++ src/components/homepage/featuredBlogs/FeaturedBlogsContainer.js
@@ -2,9 +2,36 @@ import React, { Component } from 'react';
 
 class FeaturedBlogContainer extends Component {
   render() {
-    return (
-      <div>Featured blog</div>
-    )
+    return <div id="homepage-bucketone">
+        <div className="homepage-bucketone-container">
+          <div className="blog-box">
+            <div className="blog-title">
+              <h4>Blog Title</h4>
+            </div>
+            <div className="blog-image">
+              <a href="#">read post</a>
+            </div>
+          </div>
+          <div className="blog-box">
+            <div className="blog-title">
+              <h4>Blog Title</h4>
+            </div>
+            <div className="blog-image">
+              <img src="" alt="" />
+              <a href="#">read post</a>
+            </div>
+          </div>
+          <div className="blog-box">
+            <div className="blog-title">
+              <h4>Blog Title</h4>
+            </div>
+            <div className="blog-image">
+              <img src="" alt="" />
+              <a href="#">read post</a>
+            </div>
+          </div>
+        </div>
+      </div>;
   }
 }
 
