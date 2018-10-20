6ed9400e83a86ff2b1d68850fdc564f525c3324e
Terry Dinh <tedinh@gmail.com>
2018-10-20 17:39:31 -0500

    home buckets

--- src/components/homepage/homeBucketOne/HomeBucketOneContainer.js
+++ src/components/homepage/homeBucketOne/HomeBucketOneContainer.js
@@ -2,7 +2,19 @@ import React, { Component } from 'react';
 
 class HomeBucketOneContainer extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div id="home-bucket-one">
+        <div className="img-left">
+
+        </div>
+        <div className="text-right">
+          <h1>Read. Learn. Progress.</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis dolorem asperiores inventore, quo ipsum quas laborum totam blanditiis odit maiores possimus quae nesciunt eligendi, voluptatum repellat! Minus saepe repellat accusantium.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente mollitia corporis eaque suscipit nesciunt, maxime vitae non veritatis delectus, voluptatem autem reiciendis! Praesentium laudantium vero dolor natus! Asperiores, deleniti vel!</p>
+          <a href="#" className="button">Read Our Blogs</a>
+        </div>
+      </div>
+    )
   }
 }
 
--- src/components/homepage/homeBucketTwo/HomeBucketTwoContainer.js
+++ src/components/homepage/homeBucketTwo/HomeBucketTwoContainer.js
@@ -3,7 +3,25 @@ import React, { Component } from 'react';
 class HomeBucketTwoContainer extends Component {
   render() {
     return (
-      <div>homebucket2</div>
+      <div id="home-bucket-two">
+        <div className="text-left">
+          <h1>How to Get Started.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Perspiciatis dolorem asperiores inventore, quo ipsum quas
+            laborum totam blanditiis odit maiores possimus quae nesciunt
+            eligendi, voluptatum repellat! Minus saepe repellat accusantium.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Sapiente mollitia corporis eaque suscipit nesciunt, maxime vitae
+            non veritatis delectus, voluptatem autem reiciendis! Praesentium
+            laudantium vero dolor natus! Asperiores, deleniti vel!
+          </p>
+          <a href="#" className="button">Helpful Resources</a>
+        </div>
+        <div className="img-right" />
+      </div>
     )
   }
 }
