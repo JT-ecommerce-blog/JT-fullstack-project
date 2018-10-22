0a0edc1b47ef646e5cf211acd148329d3d761c3b
Terry Dinh <tedinh@gmail.com>
2018-10-22 18:44:46 -0500

    added cors in case of cross origin restrictions

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { json } = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 // const path = require("path");
 
-
+app.use(cors());
 app.use(json());
 
 app.get('/api/customers', (req, res) => {
