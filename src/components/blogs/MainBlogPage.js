5c9d7f4191cf9d9474b533d0f0f0fafa8fb96493
Jeff Horner <jeffnhorner@outlook.com>
2018-11-07 12:23:22 -0600

    updated blog components

---
+++ src/components/blogs/MainBlogPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MainBlogPage extends Component {
+  render() {
+    return(
+      <h1>Main Blog Page Content</h1>
+    );
+  }
+}
+
+export default MainBlogPage;
